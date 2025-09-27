8d9f9d629a5a612d563e223558d410c8f0996442
azawat7 <az4wat@gmail.com>
2025-09-27 22:20:49 +0200

    add scripts and fixes

--- bot/src/base/CustomBaseClient.ts
+++ bot/src/base/CustomBaseClient.ts
@@ -9,6 +9,7 @@ import { glob } from "glob";
 import { BaseCommand, BaseEvent } from "@/base";
 import { DatabaseManager } from "@shaw/database";
 import { logger } from "@shaw/utils";
+import path from "path";
 
 export class CustomBaseClient extends Client {
   commands: Collection<String, BaseCommand>;
@@ -38,47 +39,52 @@ export class CustomBaseClient extends Client {
     await this.login(process.env.TOKEN);
   }
 
+  // TODO MAKE IT DEV ENV COMPATIBLE
+
   private async loadCommands() {
-    const commandFiles = await glob("./src/commands/**/*.ts");
     const slashCommands: SlashCommandBuilder[] = [];
 
-    for (let filePath of commandFiles) {
-      const pathParts = filePath.split("\\");
-      const commandPath = `../commands/${pathParts[pathParts.length - 2]}/${
-        pathParts[pathParts.length - 1]
-      }`;
+    const commandFiles = await glob("commands/**/*.js", {
+      cwd: path.join(__dirname, ".."),
+    });
 
-      const CommandClass = require(commandPath).default;
-      const CommandInstance: BaseCommand = new CommandClass(this);
-
-      this.commands.set(CommandInstance.commandInfo.name, CommandInstance);
-      slashCommands.push(CommandInstance.commandInfo);
+    for (const file of commandFiles) {
+      try {
+        const fullPath = path.join(__dirname, "..", file);
+        const CommandClass = require(fullPath).default;
+        const command = new CommandClass(this) as BaseCommand;
+        this.commands.set(command.commandInfo.name, command);
+        slashCommands.push(command.commandInfo);
+      } catch (error) {
+        logger.error(`Failed to load command ${file}:`, error);
+      }
     }
 
-    this.on("clientReady", async () => {
-      const guild = this.guilds.cache.get(process.env.GUILDID!);
-      if (guild) await guild.commands.set(slashCommands);
+    this.once("clientReady", async () => {
+      const guild = this.guilds.cache.get(process.env.GUILDID!)!;
+      await guild.commands.set(slashCommands);
     });
 
     logger.info(`${this.commands.size} Slash Commands loaded`);
   }
 
   private async loadEvents() {
-    const eventFiles = await glob("./src/events/**/*.ts");
+    const eventFiles = await glob("events/**/*.js", {
+      cwd: path.join(__dirname, ".."),
+    });
 
-    for (let filePath of eventFiles) {
-      const pathParts = filePath.split("\\");
-      const eventPath = `../events/${pathParts[pathParts.length - 2]}/${
-        pathParts[pathParts.length - 1]
-      }`;
-
-      const EventClass = require(eventPath).default;
-      const EventInstance: BaseEvent = new EventClass(this);
-
-      this.events.set(EventInstance.name, EventInstance);
-
-      this.on(EventInstance.name, (...args) => EventInstance.run(...args));
+    for (const file of eventFiles) {
+      try {
+        const fullPath = path.join(__dirname, "..", file);
+        const EventClass = require(fullPath).default;
+        const event = new EventClass(this) as BaseEvent;
+        this.events.set(event.name, event);
+        this.on(event.name, (...args) => event.run(...args));
+      } catch (error) {
+        logger.error(`Failed to load event ${file}:`, error);
+      }
     }
+
     logger.info(`${this.events.size} Events loaded`);
   }
 }
--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,5 +1,27 @@
 import "dotenv/config";
 import { CustomBaseClient } from "@/base";
+import { logger } from "@shaw/utils";
 
-const Client = new CustomBaseClient();
-Client.start();
+function validateEnvironment() {
+  const required = ["TOKEN", "MONGODB_URI", "GUILDID", "NODE_ENV"];
+  const missing = required.filter((key) => !process.env[key]);
+  if (missing.length > 0) {
+    logger.error("Missing required environment variables:");
+    missing.forEach((key) => logger.error(`- ${key}`));
+    logger.error("Please check your .env file in the root directory");
+    process.exit(1);
+  }
+}
+
+async function startBot() {
+  try {
+    validateEnvironment();
+    const Client = new CustomBaseClient();
+    await Client.start();
+  } catch (error) {
+    logger.error("Error:", error);
+    process.exit(1);
+  }
+}
+
+startBot();
